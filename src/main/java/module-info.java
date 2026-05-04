f7655c15cf7ef05643ee1c28da76e3467ce82c2b
Juan David Tapiero <juand.tapierom@uqvirtual.edu.co>
2026-05-04 00:20:48 -0500

    Fix: corrección de module info y pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module co.edu.uniquindio.tallerbeta {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires lombok;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.xml;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires java.desktop;
 
     opens co.edu.uniquindio.tallerbeta to javafx.fxml;
+    opens co.edu.uniquindio.tallerbeta.model.entity to com.fasterxml.jackson.databind;
+    opens co.edu.uniquindio.tallerbeta.model.Enum to com.fasterxml.jackson.databind;
+
     exports co.edu.uniquindio.tallerbeta;
 }
