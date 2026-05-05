164030ee84049b4d31a071cc88796ccda3b050d4
Jose Bedoya <josem.bedoyam@uqvirtual.edu.co>
2026-05-05 17:38:58 -0500

    fix: Abrir el login

--- src/main/java/co/edu/uniquindio/tallerbeta/HelloApplication.java
+++ src/main/java/co/edu/uniquindio/tallerbeta/HelloApplication.java
@@ -8,12 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws IOException {;
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Taller BeTa — Registrar Orden de Mantenimiento");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
